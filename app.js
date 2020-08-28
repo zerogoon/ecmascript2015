749753c0268dc5ab5b37cdc3585ff6ed9b494697
zerogoon <kckzero@gmail.com>
2020-08-28 18:20:45 +0900

    fix

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const user = {
-    NAME: "nico",
-    age: 24,
-    password: 12345
-};
+const p1 = new Promise(resolve => {
+    setTimeout(resolve, 10000, "First");
+});
 
-const rename = ({NAME: name, ...rest}) => ({name, ...rest});
+const p2 = new Promise((resolve, reject) => {
+    setTimeout(reject, 5000, "I hate JS");
+});
 
-console.log(rename(user));
+const p3 = new Promise(resolve => {
+    setTimeout(resolve, 3000, "Third");
+});
+
+const motherPromise = Promise.race([p1, p2, p3]);
+
+motherPromise
+    .then(values => console.log(values))
+    .catch(err => console.log(err));
