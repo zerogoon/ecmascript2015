637b1dc0258dddc74a9c523dbea46cb146c6fcf8
zerogoon <kckzero@gmail.com>
2020-09-02 16:59:41 +0900

    section12.1

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const friends = ["Dal", "Flynn", "Mark", "Godkimchi", "Japan Guy"];
+const userObj = {
+    username: "nico",
+    age: 12,
+    password: 1234
+};
 
-function* friendTeller() {
-    for (const friend of friends) {
-        yield friend;
+const userFilter = {
+    get: (target, prop, receiver) => {
+        return prop === "password" ? `${"*".repeat(5)}` : target[prop];
+    },
+    set: () => {
+        console.log("Somebody wrote something");
+    },
+    deleteProperty: (target, prop) => {
+        if (prop === "password") {
+            return;
+        } else {
+            target[prop] = "DELETED";
+        }
     }
-}
+};
 
-const friendLooper = friendTeller();
+const filteredUser = new Proxy(userObj, userFilter);
