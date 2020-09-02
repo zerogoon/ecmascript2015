e01e1cb1e4fe16ed4243dd7905261930993e3a42
zerogoon <kckzero@gmail.com>
2020-09-02 16:43:56 +0900

    section12.0

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-const map = new Map();
+const friends = ["Dal", "Flynn", "Mark", "Godkimchi", "Japan Guy"];
+
+function* friendTeller() {
+    for (const friend of friends) {
+        yield friend;
+    }
+}
+
+const friendLooper = friendTeller();
