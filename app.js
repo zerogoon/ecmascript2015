0f0746c373b78f0b5ba559d1cfb2347273780686
zerogoon <kckzero@gmail.com>
2020-09-02 11:23:46 +0900

    fix_200902_002

--- app.js
+++ app.js
@@ -1,23 +1,48 @@
-const getMoviesPromise = () => {
-    fetch("https://yts.am/api/v2/list_movies.json")
-        .then(response => {
-            console.log(response);
-            return response.json();
-        })
-        .then(json => console.log(json))
-        .catch(e => console.log(`✖️ ${e}`));
-};
-
-const getMoviesAsync = async () => {
-    try {
-        const response = await fetch("https://yts.am/api/v2/list_movies.json");
-        const json = await response.json();
-        console.log(json);
-    } catch (e) {
-        console.log(`✖️ ${e}`);
-    } finally {
-        console.log("We are done!");
+class User {
+    constructor(options) {
+        this.username = options.name;
+        this.lastName = options.lastName;
+        this.email = options.email;
+        this.password = options.password;
     }
-};
+    sayHello() {
+        console.log(`Hello, my name is ${this.username}`);
+    }
+    getProfile() {
+        console.log(`${this.username} ${this.email} ${this.password}`)
+    }
+    updatePassword(newPassword, currentPassword) {
+        if (currentPassword == this.password) {
+            this.password = newPassword;
+        } else {
+            console.log("Can't change password");
+        }
+    }
+}
 
-getMoviesAsync();
+const sexyUser = new User({
+    username: "ZERO",
+    lastName: "GOON", 
+    email: "zerogoon@gmail.com", 
+    password: "1234"
+});
+
+class Admin extends User {
+    constructor({userName, lastName, email, password, superAdmin, isActive}) {
+        super({userName, lastName, email, password});
+        this.superadmin = superAdmin;
+        this.isActive = isActive;
+    }
+    deleteWebsite() {
+        console.log("Deleting the whole website....");
+    }
+}
+
+const admin = new Admin({
+    username: "ZERO",
+    lastName: "GOON", 
+    email: "zerogoon@gmail.com", 
+    password: "1234",
+    supserAdmin: true,
+    isActive: true
+});
